f61ce070f6679dfac5e210678294d2fa3d2273d8
Siddharth <siddkot@gmail.com>
2020-10-17 22:11:55 +0530

    All set

--- pkg_task0/scripts/node_turtle_revolve.py
+++ pkg_task0/scripts/node_turtle_revolve.py
@@ -1,30 +1,45 @@
 #!/usr/bin/env python
 
 ##Task 0
-#To do stop the simulation after one cycle
+#To know when its completed a revolution it checks the reading of theta, which if changes from negative to po
 
 
 import rospy
 
+#Check the ROS website to know from where to import which package
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 
-import sys 
+import sys
 
 
 stop_sim= False;
 first_reading= True;
+prev_angle=0
 
 
+#callback for the pose subscriber
 def pose_data(pose):
-	global stop_sim, first_reading, start_angle
-	rospy.loginfo("Angle travelled: %f", pose.theta)
+	global stop_sim, first_reading, start_angle,start_x, start_y, prev_angle
+
+	#-----------------------------Take initial readings-----------------------
 	if(first_reading):
 		start_angle=pose.theta
-	
+		start_x=pose.x
+		start_y=pose.y
+		rospy.loginfo("PositionX = %f: PositionY = %f: angle: %f", start_x, start_y, pose.theta)
+		first_reading=False
+	#------------------------------------------------------------------------
+
+	rospy.loginfo("Angle travelled: %f", pose.theta)
+	if(prev_angle<0 and pose.theta>0):
+		stop_sim=True
+	prev_angle=pose.theta
 
 
-def node_turtle_revolve(lin_vel,ang_vel):
+
+#main function
+def node_turtle_revolve(lin_vel= 3.0,ang_vel=1.5):
 
 	rospy.init_node('move_turtle', anonymous=True)
 	rospy.Subscriber('/turtle1/pose',Pose, pose_data)
@@ -33,11 +48,13 @@ def node_turtle_revolve(lin_vel,ang_vel):
 	rate = rospy.Rate(10) # refresh screen every 1/10s
 
 	vel = Twist()
-	
-	
+	radius= round(lin_vel/ang_vel)
+	rospy.loginfo("Radius of revolution: %f", radius)
+
+
 	while not rospy.is_shutdown():
 
-	
+
 		vel.linear.y = 0
 		vel.linear.z = 0
 		vel.linear.x = lin_vel
@@ -47,19 +64,17 @@ def node_turtle_revolve(lin_vel,ang_vel):
 		vel.angular.z = ang_vel
 
 
-
-		#rospy.loginfo("PositionX = %f: PositionY = %f:",posX_var, posY_var)
-
 		pub.publish(vel)
 
-		
-		
-
+		if(stop_sim):
+				rospy.loginfo("Completed Revolution")
+				break
 
 		rate.sleep()
 
 if __name__ == '__main__':
 	try:
-		node_turtle_revolve(float(sys.argv[1]),float(sys.argv[2]))
+		#node_turtle_revolve(float(sys.argv[1]),float(sys.argv[2]))
+		node_turtle_revolve()
 	except rospy.ROSInterruptException:
 		pass
